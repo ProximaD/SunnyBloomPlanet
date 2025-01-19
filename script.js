09af1a8df18c7bf13768e3240ffc76dea29abe70
Sandra Lavenda <111272273+ProximaD@users.noreply.github.com>
2025-01-19 14:26:08 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+/*=============== MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+
+/* Show Menu  */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+
+/* Hide Menu */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    //When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    //Add a class if the button offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
